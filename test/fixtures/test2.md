15464b383ed3054f564da84f6ad51de969dc5089
Blaine Price <1wbprice@gmail.com>
2018-02-20 16:23:14 -0500

    Minimally working

--- test/fixtures/test2.md
+++ test/fixtures/test2.md
@@ -2,4 +2,4 @@
 
 This is an example of a file that calls another file.
 
-{{#file './test3.md'}}{{/file}}
+{{{file './test3.md'}}}
--- test/preprocess.test.js
+++ test/preprocess.test.js
@@ -29,7 +29,7 @@ describe('Preprocess', function() {
         expect(preprocess(markdown, {name: 'Steven'})).to.include('Steven');
     });
 
-    it('shouldn\'t change the formatting of a template', function() {
+    it('shouldn\'t affect overall formatting of a template', function() {
         const output = preprocess(markdown, {name: 'Steven'});
         expect(output.replace('Steven', '{{name}}')).to.equal(markdown);
     });
@@ -51,6 +51,38 @@ describe('Preprocess file helper', function() {
             greeting: 'Buongiorno',
             emotion: 'excited'
         };
-        console.log(preprocess(markdown, context));
+        expect(preprocess(markdown, context)).to.contain("Buongiorno, my name is Steven.  I'm very excited to meet you.");
+    });
+
+    it('should insert files into other files into other files', function() {
+        const context = {
+            name: 'Steven',
+            greeting: 'Buongiorno',
+            emotion: 'excited'
+        };
+        expect(preprocess(markdown, context)).to.contain('Recursion!');
+    });
+});
+
+describe('Conditional helpers', function() {
+    let markdown;
+
+    before(function(done) {
+        fs.readFile(path.join(__dirname, 'fixtures', 'test5.md'), 'utf8', function(err, data) {
+            markdown = data;
+            done();
+        });
+    });
+
+    it('should display AWS content to AWS users', function() {
+        const context = { isAWS: true };
+        const output = preprocess(markdown, context);
+        expect(output).to.contain('AWS-specific content');
+    });
+
+    it('should display Azure content to Azure users', function() {
+        const context = { isAzure: true };
+        const output = preprocess(markdown, context);
+        expect(output).to.contain('Azure-specific content');
     });
 });
